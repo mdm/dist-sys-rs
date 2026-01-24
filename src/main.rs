c16e87eb5251f85de9066ac4956b0c145e40fd2b
Marc Dominik Migge <marcmigge@gmx.net>
2026-01-25 00:57:37 +0100

    Implement Echo challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdin;
+
+use crate::message::{MsgEnvelope, MsgPayload};
+
+mod message;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut msg_id = 1;
+    let mut buffer = String::new();
+    loop {
+        let len = stdin().read_line(&mut buffer)?;
+        if len == 0 {
+            eprintln!("Detected EOF, exiting.");
+            break;
+        }
+        eprintln!("Received request ({} bytes): {}", len, buffer);
+        let parsed = serde_json::from_str::<message::MsgEnvelope<'_>>(&buffer).unwrap();
+        let response: MsgEnvelope<'_> = match parsed.body.payload {
+            MsgPayload::Init { .. } => parsed.reply(msg_id, MsgPayload::InitOk),
+            MsgPayload::Echo { echo } => parsed.reply(
+                msg_id,
+                MsgPayload::EchoOk {
+                    echo: echo.to_string(),
+                },
+            ),
+            _ => panic!("Unexpected message type"),
+        };
+        let serialized = serde_json::to_string(&response)?;
+        println!("{}", serialized);
+        msg_id += 1;
+        buffer.clear();
+    }
+    Ok(())
 }
---
+++ src/message.rs
@@ -0,0 +1,197 @@
+use std::{fmt::Display, str::FromStr};
+
+use serde::{Deserialize, Serialize, de::Visitor};
+
+#[derive(Debug)]
+pub enum EntityId {
+    Client(usize),
+    Node(usize),
+}
+
+impl Display for EntityId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EntityId::Client(id) => write!(f, "c{}", id),
+            EntityId::Node(id) => write!(f, "n{}", id),
+        }
+    }
+}
+
+impl Serialize for EntityId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.collect_str(self)
+    }
+}
+
+impl FromStr for EntityId {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() < 2 {
+            return Err("EntityId too short");
+        }
+
+        let (prefix, id) = s.split_at_checked(1).ok_or("Invalid EntityId format")?;
+
+        let id = id
+            .parse::<usize>()
+            .map_err(|_| "Invalid number for EntityId")?;
+
+        match prefix {
+            "c" => Ok(EntityId::Client(id)),
+            "n" => Ok(EntityId::Node(id)),
+            _ => Err("Invalid EntityId prefix"),
+        }
+    }
+}
+
+struct EntityIdVisitor;
+
+impl<'de> Visitor<'de> for EntityIdVisitor {
+    type Value = EntityId;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, r#"a string like "c42" or "n7""#)
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        v.parse().map_err(serde::de::Error::custom)
+    }
+}
+
+impl<'de> Deserialize<'de> for EntityId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_str(EntityIdVisitor)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub enum MsgPayload<'p> {
+    Init {
+        node_id: EntityId,
+        node_ids: Vec<EntityId>,
+    },
+    InitOk,
+    Echo {
+        echo: &'p str,
+    },
+    EchoOk {
+        echo: String,
+    },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MsgBody<'b> {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) msg_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(crate) in_reply_to: Option<usize>,
+    #[serde(borrow, flatten)]
+    pub(crate) payload: MsgPayload<'b>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MsgEnvelope<'e> {
+    pub(crate) src: EntityId,
+    pub(crate) dest: EntityId,
+    #[serde(borrow)]
+    pub(crate) body: MsgBody<'e>,
+}
+
+impl MsgEnvelope<'_> {
+    pub fn reply<'r>(self, msg_id: usize, payload: MsgPayload<'r>) -> MsgEnvelope<'r> {
+        MsgEnvelope {
+            src: self.dest,
+            dest: self.src,
+            body: MsgBody {
+                msg_id: Some(msg_id),
+                in_reply_to: self.body.msg_id,
+                payload,
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deserialize_init_request() {
+        let envelope = serde_json::from_str::<MsgEnvelope<'_>>(
+            r#"{
+            "src": "c1",
+            "dest": "n1",
+            "body": {
+                "type": "init",
+                "msg_id": 1,
+                "node_id": "n1",
+                "node_ids": ["n1", "n2", "n3"]
+            }
+        }"#,
+        );
+
+        assert!(envelope.is_ok());
+    }
+
+    #[test]
+    fn test_deserialize_init_response() {
+        let envelope = serde_json::from_str::<MsgEnvelope<'_>>(
+            r#"{
+            "src": "n1",
+            "dest": "c1",
+            "body": {
+                "type": "init_ok",
+                "in_reply_to": 1
+            }
+        }"#,
+        );
+
+        assert!(envelope.is_ok());
+    }
+
+    #[test]
+    fn test_deserialize_echo_request() {
+        let envelope = serde_json::from_str::<MsgEnvelope<'_>>(
+            r#"{
+            "src": "c1",
+            "dest": "n1",
+            "body": {
+                "type": "echo",
+                "msg_id": 1,
+                "echo": "Please echo 35"
+            }
+        }"#,
+        );
+
+        assert!(envelope.is_ok());
+    }
+
+    #[test]
+    fn test_deserialize_echo_response() {
+        let envelope = serde_json::from_str::<MsgEnvelope<'_>>(
+            r#"{
+            "src": "n1",
+            "dest": "c1",
+            "body": {
+                "type": "echo_ok",
+                "msg_id": 1,
+                "in_reply_to": 1,
+                "echo": "Please echo 35"
+            }
+        }"#,
+        );
+
+        assert!(envelope.is_ok());
+    }
+}
